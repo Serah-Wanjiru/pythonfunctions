1cc7444f0d316ae3cbe595d4386063673eccc924
serah wanjiru <WanjiruMburuuu@gmail.com>
2023-03-31 16:18:29 +0300

    pythonfunctions

---
+++ math.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    answer= a+b
+    return answer
+
+def subtract(x,y):
+    difference =x-y
+    return difference 
+
+def divide(num,num1):
+    answer=num/num1
+    return answer
+
+def multiply(num4,num5):
+    answer =num4*num5
+    return answer
+
+def remainder(num2,num3):
+    answer =num2%num3
+    return answer
+        
+               
